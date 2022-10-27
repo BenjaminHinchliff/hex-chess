ed068c179a6f58bd458e9112e33bd587d99d160e
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2022-10-26 23:27:20 -0700

    add main loop basics

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
+use std::io::{self, BufRead, Write};
+
 use hex_chess::HexBoard;
 
 pub fn main() {
-    let board = HexBoard::new_initialize();
+    let mut board = HexBoard::new_initialize();
 
-    println!("{}", board);
+    let mut lines = io::stdin().lock().lines();
+
+    loop {
+        println!("{}", board);
+
+        print!("Move: ");
+        io::stdout().flush().expect("unable to flush output");
+
+        let _move = match lines.next() {
+            Some(_move) => _move,
+            None => break,
+        };
+
+        let _move = _move.expect("unable to read input");
+        let _move = _move
+            .splitn(2, "->")
+            .map(|c| {
+                c.trim()
+                    .splitn(2, ",")
+                    .map(|n| n.trim().parse::<i32>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        let (f, t) = if let [f, t] = &_move[..] {
+            (f, t)
+        } else {
+            unreachable!()
+        };
+
+        board
+            .move_piece((f[0], f[1]).into(), (t[0], t[1]).into())
+            .unwrap();
+    }
 }
